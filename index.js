68ea4e1243b788f2236e0a15ca22a71cfa16b5c2
Alexandre <alexandrepj13@gmail.com>
2022-04-27 11:59:47 -0300

    Adicionando funções nos botões da calculadora

---
+++ index.js
@@ -0,0 +1,25 @@
+function insert(num){
+ var numero = document.querySelector(".resultado-calculadora").innerHTML;
+ document.querySelector(".resultado-calculadora").innerHTML = numero + num
+}
+
+
+document.querySelector(".reset").addEventListener("click",()=>{
+  document.querySelector(".resultado-calculadora").innerHTML=""
+
+})
+
+
+document.querySelector(".delete").addEventListener("click",()=>{
+  var resultado = document.querySelector(".resultado-calculadora").innerHTML;
+  document.querySelector(".resultado-calculadora").innerHTML = resultado.substring(0, resultado.length-1)
+})
+
+document.querySelector(".igual").addEventListener("click",()=>{
+  var resultado = document.querySelector(".resultado-calculadora").innerHTML;
+  if(resultado){
+    document.querySelector(".resultado-calculadora").innerHTML = eval(resultado);
+  }  else{
+
+  } 
+})
