9fec594d2fa54b5fdc18d32403f5d6fe23f37932
Alexandre <alexandrepj13@gmail.com>
2022-06-22 09:00:13 -0300

    Arrumando codigo

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function insert(num){
- var numero = document.querySelector(".resultado-calculadora").innerHTML;
- document.querySelector(".resultado-calculadora").innerHTML = numero + num
+function insert(num) {
+  var numero = document.querySelector(".resultado-calculadora").innerHTML;
+  document.querySelector(".resultado-calculadora").innerHTML = numero + num
 }
 
 
-document.querySelector(".reset").addEventListener("click",()=>{
-  document.querySelector(".resultado-calculadora").innerHTML=""
+document.querySelector(".reset").addEventListener("click", () => {
+  document.querySelector(".resultado-calculadora").innerHTML = ""
 
 })
 
 
-document.querySelector(".delete").addEventListener("click",()=>{
+document.querySelector(".delete").addEventListener("click", () => {
   var resultado = document.querySelector(".resultado-calculadora").innerHTML;
-  document.querySelector(".resultado-calculadora").innerHTML = resultado.substring(0, resultado.length-1)
+  document.querySelector(".resultado-calculadora").innerHTML = resultado.substring(0, resultado.length - 1)
 })
 
-document.querySelector(".igual").addEventListener("click",()=>{
+document.querySelector(".igual").addEventListener("click", () => {
   var resultado = document.querySelector(".resultado-calculadora").innerHTML;
-  if(resultado){
+  if (resultado) {
     document.querySelector(".resultado-calculadora").innerHTML = eval(resultado);
-  }  else{
+  } else {
 
-  } 
+  }
 })
